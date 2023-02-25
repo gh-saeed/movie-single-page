b041cf0edcc67a5bd2e71a66fcd2c7f13eb4b9a0
saeed <ghasemkhani.saeed@gmail.com>
2023-02-25 19:09:50 +0330

    #

---
+++ src/components/contentWrapper/ContentWrapper.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./style.scss";
+
+const ContentWrapper = ({ childern }) => {
+    return <div className="contrntWrapper">{childern}</div>;
+};
+
+export default ContentWrapper;
